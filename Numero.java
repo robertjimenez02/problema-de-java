729b1d9f746e4e8bb3e3d8a287cfdb74cf09b4cf
Roberto Jiménez Ortiz <62734320+robertjimenez02@users.noreply.github.com>
2020-03-26 22:07:31 -0600

    Add files via upload

---
+++ Numero.java
@@ -0,0 +1,41 @@
+public abstract class Numero{
+	
+protected int numeroA;
+protected int numeroB;
+protected int numeroC;
+protected int numeroD;
+protected int sumar;
+protected int restar;
+protected int multiplicarPor;
+protected int dividirPor;
+
+	public void setsumar(int n){
+		this.numeroA=n;
+       
+    }
+    public int getsumar(){
+    	return this.numeroA;
+    }
+
+	public void setrestar(int n){
+        this.numeroB=n;
+		 
+	}
+	public int getrestar(){
+        return this.numeroB;
+	}
+	public void setmultiplicarPor(int n){
+		 this.numeroC=n;
+
+	}
+	public int getmultiplicarPor(){
+		return this.numeroC;
+	}
+	public void setdividirPor(int n){
+		this.
+		 
+}
+      public String toString(){
+      	return "Numero: ";
+      }
+}
